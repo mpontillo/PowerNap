2656e6fab1e7e188d8580a90153418033451e470
Mike Pontillo <pontillo@gmail.com>
2011-04-27 18:25:58 -0700

    Add test cases for NH-2667

---
+++ nhibernate/src/NHibernate.Test/NHSpecificTest/NH2667/Model.cs
@@ -0,0 +1,9 @@
+namespace NHibernate.Test.NHSpecificTest.NH2667
+{
+    public class Info
+    {
+        public virtual int Id { get; set; }
+        public virtual int? Version { get; set; }
+        public virtual string Data { get; set; }
+    }
+}
